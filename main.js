a800f17f67bc04640f7f72092de05aeeb1367c80
Saurabh Mhaisagar <smhaisagar026@gmail.com>
2024-02-15 17:25:40 +0530

    final commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// for sticky navbar
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+//show hide FAQ Answer
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    //change icon answer
+    const icon = faq.querySelector(".faq_icon i");
+    if (icon.className === "fa-solid fa-plus") {
+      icon.className = "fa-solid fa-minus";
+    } else {
+      icon.className = "fa-solid fa-plus";
+    }
+  });
+});
+
+// show//hide nav menu
+
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+//close nav menu
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+closeBtn.addEventListener("click", closeNav);
